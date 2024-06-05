d65795505418449e9e05d2140a560c6c4ddc960f
IonosWebbApp <ionos6286@gmail.com>
2024-06-05 11:36:55 -0500

    tablas de calculos
    tablas csv

--- mi_proyecto/app/app.py
+++ mi_proyecto/app/app.py
@@ -1,18 +1,68 @@
+# app.py
+
 from flask import Flask, render_template, request, redirect, url_for, send_file
 import os
 from werkzeug.utils import secure_filename
 import pandas as pd
-import plotly.graph_objs as go
 import zipfile
 import io
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads/'
-app.config['GRAPH_FOLDER'] = 'static/'
 ALLOWED_EXTENSIONS = {'csv'}
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def process_csv(file_path):
+    data = pd.read_csv(file_path)
+    dates = []
+    num_equity_actions = 0
+    num_equity_options = 0
+    symbol_counts = {}
+    symbol_total_stock = {}
+    for column in data.columns:
+        if data[column].dtype == 'object':
+            try:
+                date_column = pd.to_datetime(data[column], errors='coerce')
+                date_column = date_column.dropna()
+                if not date_column.empty:
+                    sorted_dates = sorted(date_column)
+                    first_date = sorted_dates[0]
+                    last_date = sorted_dates[-1]
+                    total_days = (last_date - first_date).days
+                    if total_days < 0:
+                        total_days = abs(total_days)  # Tomar el valor absoluto si es negativo
+                    dates.append(first_date)  # Añadir la primera fecha
+                    dates.append(last_date)  # Añadir la última fecha
+                    dates.append(total_days)  # Añadir el total de días transcurridos
+            except ValueError:
+                pass
+    # Calcular el número de acciones del tipo "Equity" entre las fechas de "First Date" y "Last Date"
+    if 'Instrument Type' in data.columns:
+        equity_data = data[(data['Instrument Type'] == 'Equity') & (pd.to_datetime(data['Date']) >= dates[0]) & (pd.to_datetime(data['Date']) <= dates[1])]
+        num_equity_actions = equity_data.shape[0]
+        # Calcular el número de operaciones del tipo "Equity Option" entre las fechas de "First Date" y "Last Date"
+        equity_option_data = data[(data['Instrument Type'] == 'Equity Option') & (pd.to_datetime(data['Date']) >= dates[0]) & (pd.to_datetime(data['Date']) <= dates[1])]
+        num_equity_options = equity_option_data.shape[0]
+        # Calcular el conteo de símbolos y el total de stock para acciones de tipo "Equity"
+        for _, row in equity_data.iterrows():
+            symbol = row['Symbol']
+            quantity = row['Quantity']
+            if symbol in symbol_counts:
+                symbol_counts[symbol] += 1
+                symbol_total_stock[symbol] += quantity
+            else:
+                symbol_counts[symbol] = 1
+                symbol_total_stock[symbol] = quantity
+    return data, dates, num_equity_actions, num_equity_options, symbol_counts, symbol_total_stock
+
+def get_equity_symbols(data):
+    if 'Symbol' in data.columns and 'Instrument Type' in data.columns:
+        equity_symbols = data[data['Instrument Type'] == 'Equity']['Symbol'].unique().tolist()
+        return equity_symbols
+    return []
 
 @app.route('/')
 def index():
@@ -29,75 +79,24 @@ def upload_file():
         filename = secure_filename(file.filename)
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(file_path)
-        data, descriptions = process_csv(file_path)
-        generate_graphs(data, filename, descriptions)
         return redirect(url_for('uploaded_file', filename=filename))
     return redirect(request.url)
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    graph_files = []
-    descriptions = []
-    base_filename = 'graph_' + filename[:-4]
-    for file in os.listdir(app.config['GRAPH_FOLDER']):
-        if file.startswith(base_filename):
-            graph_files.append(url_for('static', filename=file))
-            column_name = file.split('_')[-1][:-5]
-            descriptions.append(f"Histogram of the column '{column_name}' showing the distribution of values.")
-    return render_template('uploaded.html', filename=filename, graph_files=graph_files, descriptions=descriptions)
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    _, dates, num_equity_actions, num_equity_options, symbol_counts, symbol_total_stock = process_csv(file_path)
+    data = pd.read_csv(file_path)
+    equity_symbols = get_equity_symbols(data)
+    # Formatear las fechas en el formato deseado
+    for i in range(2):
+        dates[i] = dates[i].strftime('%Y-%m-%d')
+    return render_template('uploaded.html', filename=filename, dates=dates, num_equity_actions=num_equity_actions, num_equity_options=num_equity_options, symbol_counts=symbol_counts, symbol_total_stock=symbol_total_stock, equity_symbols=equity_symbols)
 
 @app.route('/download/<filename>')
-def download_graphs(filename):
-    zip_buffer = io.BytesIO()
-    with zipfile.ZipFile(zip_buffer, 'w') as zf:
-        for file in os.listdir(app.config['GRAPH_FOLDER']):
-            if file.startswith('graph_' + filename[:-4]):
-                zf.write(os.path.join(app.config['GRAPH_FOLDER'], file), file)
-    zip_buffer.seek(0)
-    return send_file(zip_buffer, as_attachment=True, download_name=f"graphs_{filename[:-4]}.zip")
-
-def process_csv(file_path):
-    data = pd.read_csv(file_path)
-    descriptions = {column: f"Histogram of the column '{column}' showing the distribution of values." for column in data.columns}
-    return data, descriptions
-
-def generate_graphs(data, filename, descriptions):
-    generated_graphs = set()  # Conjunto para almacenar las columnas ya representadas en un gráfico
-
-    for column in data.columns:
-        # Verificar si los datos de la columna ya se han representado en otro gráfico
-        if tuple(data[column].to_list()) in generated_graphs:
-            continue
-
-        # Si no se ha representado antes, generar el gráfico y agregar la columna al conjunto de datos generados
-        trace = go.Histogram(
-            x=data[column],
-            name=column,
-            marker=dict(color=next_color())
-        )
-        layout = go.Layout(
-            title=f'Histogram of {column}',
-            xaxis=dict(title='Value'),
-            yaxis=dict(title='Frequency'),
-            barmode='overlay',
-            legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1)
-        )
-        fig = go.Figure(data=[trace], layout=layout)
-        fig.write_html(os.path.join(app.config['GRAPH_FOLDER'], f"graph_{filename[:-4]}_{column}.html"))
-
-        generated_graphs.add(tuple(data[column].to_list()))  # Agregar la lista de datos al conjunto de datos generados
-
-color_idx = 0
-def next_color():
-    global color_idx
-    colors = [
-        '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', 
-        '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', 
-        '#bcbd22', '#17becf'
-    ]
-    color = colors[color_idx % len(colors)]
-    color_idx += 1
-    return color
+def download_csv(filename):
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    return send_file(file_path, as_attachment=True, download_name=filename)
 
 if __name__ == "__main__":
     app.run(debug=True)
